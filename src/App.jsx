3f284cd980e514a43f75cadccbf0ccff76cd0f83
Aayush Kumar <aayushagrawal296@gmail.com>
2024-08-16 19:25:01 +0530

    chatApp_frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import Chat from "./components/chat/Chat"
+import List from "./components/list/List"
+import Detail from "./components/detail/Detail"
+
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+   {/* <h1>Hello guys</h1> */}
+   <List/>
+   <Chat/>
+   <Detail/>
+
+    </div>
   )
 }
 
